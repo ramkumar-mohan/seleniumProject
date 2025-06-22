254fa4842385e20ed541ad491f6047d25a7cff33
unknown <ranger3800@gmail.com>
2025-06-22 12:52:22 +0530

    FINAL commit

---
+++ treeset_FINAL.java
@@ -0,0 +1,18 @@
+package ramdatastructures;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class treeset {
+
+	public static void main(String[] args) {
+		TreeSet<Integer> numbers = new TreeSet<>();
+		Scanner scanner = new Scanner(System.in);
+		for (int i=0; i<=4; i++)
+		{
+			System.out.println("Enter the Numbers:");
+			Integer obj1 = scanner.nextInt();
+			numbers.add(obj1);
+		}
+		System.out.println("Final Sorted Numbers are 1:" + numbers);
+	}
+}
